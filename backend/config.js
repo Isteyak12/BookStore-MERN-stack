07a1bf1a8da90c069796fd64dcd4ff91c24401a6
isteyak <isteyakislam12@gmail.com>
2024-10-15 16:40:46 -0400

    first commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT=5555;
+export const mongoDBURL='mongodb+srv://root:b1RhmoEqqvtIXjZC@books-store-mern.qmvtn.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
